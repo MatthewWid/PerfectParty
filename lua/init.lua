2d20c55af754b2df087e31f2fed61092ea74acd0
MatthewWid <115mattey@gmail.com>
2018-10-25 17:11:07 +1100

    Added '!pname' command. Remove unnecessary brackets. Replaced unnecessary return statements with a single ending one.

--- lua/init.lua
+++ lua/init.lua
@@ -82,24 +82,25 @@ function findPlayer(pName)
 	return false;
 end
 hook.Add("PlayerSay", "Party Commands", function(ply, text)
+	local wasCommand = false;
 	if isCommand(text, "pcreate") then
+		wasCommand = true;
 		Party:New(string.sub(text, 10), ply);
-
-		return "";
 	end
 
-	if (isCommand(text, "pleave")) then
+	if isCommand(text, "pleave") then
+		wasCommand = true;
 		if not ply.CurrentParty then
 			ply:ChatPrint("You are not in a party.");
+			wasCommand = true;
 			return "";
 		end
 
 		ply.CurrentParty:RemovePlayer(ply, true);
-
-		return "";
 	end
 
-	if (isCommand(text, "ppromote")) then
+	if isCommand(text, "ppromote") then
+		wasCommand = true;
 		if not ply.CurrentParty then
 			ply:ChatPrint("You are not in a party.");
 			return "";
@@ -122,11 +123,10 @@ hook.Add("PlayerSay", "Party Commands", function(ply, text)
 		ply.CurrentParty.leader = playerToAdd;
 		ply:ChatPrint("You have promoted " .. playerToAdd:Nick() .. " to the party leader.");
 		playerToAdd:ChatPrint("You have been promoted to the party leader.");
-
-		return "";
 	end
 
-	if (isCommand(text, "pinvite")) then
+	if isCommand(text, "pinvite") then
+		wasCommand = true;
 		if not ply.CurrentParty then
 			ply:ChatPrint("You are not in a party.");
 			return "";
@@ -136,12 +136,29 @@ hook.Add("PlayerSay", "Party Commands", function(ply, text)
 			return "";
 		end
 	end
+
+	if isCommand(text, "pname") then
+		wasCommand = true;
+		if not ply.CurrentParty then
+			ply:ChatPrint("You are not in a party.");
+			return "";
+		end
+		if not ply.CurrentParty.leader == ply then
+			ply:ChatPrint("You are not the party leader.");
+			return "";
+		end
+
+		ply.CurrentParty.name = string.sub(text, 8);
+		ply:ChatPrint("You renamed the party to '" .. ply.CurrentParty.name .. "'.");
+	end
+
+	if wasCommand then return ""; end;
 end);
 
 
-player.GetAll()[2]:Say("!pcreate The Mobsters");
-timer.Simple(1, function()
-	AllParties[1]:AddPlayer(player.GetAll()[1], true);
+-- player.GetAll()[2]:Say("!pcreate The Mobsters");
+-- timer.Simple(1, function()
+-- 	AllParties[1]:AddPlayer(player.GetAll()[1], true);
 
-	player.GetAll()[2]:Say("!ppromote Mob");
-end);
+-- 	player.GetAll()[2]:Say("!ppromote Mob");
+-- end);
